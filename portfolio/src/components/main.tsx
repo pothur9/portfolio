20aa56610638b4ae285ec5061b6de1892fe424d9
pothur9 <chowdaryp697@gmail.com>
2024-01-08 18:45:48 +0530

    projects completed

--- portfolio/src/components/main.tsx
+++ portfolio/src/components/main.tsx
@@ -1,4 +1,8 @@
 import "./main.css";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
 
 function Main() {
   return (
@@ -40,34 +44,111 @@ function Main() {
           for more details.
         </p>
         <div className="tech">
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" style={{width:'60px',margin:'20px'}} />
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" style={{width:'60px',margin:'20px'}}/>
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" style={{width:'60px',margin:'20px'}}/>
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg"  style={{width:'60px',margin:'20px'}}/>        
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" style={{width:'60px',margin:'20px'}}/> 
-       
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" style={{width:'60px',margin:'20px'}}/>
-          
-        </div>     
-        <div className="tech" style={{marginTop:'-20px'}}>
-        
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" style={{width:'60px',margin:'20px'}}/>
-              
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg" style={{width:'60px',margin:'20px'}}/>
-        
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg" style={{width:'60px',margin:'20px'}}/>
-        
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg" style={{width:'60px',margin:'20px'}}/>
-        
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain-wordmark.svg" style={{width:'60px',margin:'20px'}}/>
-        
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original-wordmark.svg" style={{width:'60px',margin:'20px'}}/>
-          
-          
-          
-          
-          
-        </div> 
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+            style={{ width: "60px", margin: "20px" }}
+          />
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
+            style={{ width: "60px", margin: "20px" }}
+          />
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
+            style={{ width: "60px", margin: "20px" }}
+          />
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg"
+            style={{ width: "60px", margin: "20px" }}
+          />
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg"
+            style={{ width: "60px", margin: "20px" }}
+          />
+
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg"
+            style={{ width: "60px", margin: "20px" }}
+          />
+        </div>
+        <div className="tech" style={{ marginTop: "-20px" }}>
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
+            style={{ width: "60px", margin: "20px" }}
+          />
+
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg"
+            style={{ width: "60px", margin: "20px" }}
+          />
+
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg"
+            style={{ width: "60px", margin: "20px" }}
+          />
+
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg"
+            style={{ width: "60px", margin: "20px" }}
+          />
+
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain-wordmark.svg"
+            style={{ width: "60px", margin: "20px" }}
+          />
+
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original-wordmark.svg"
+            style={{ width: "60px", margin: "20px" }}
+          />
+        </div>
+        <h1 className="work">My work</h1>
+        <p className="work-text">
+          Deployed scalable travel, event and telemedicine web and hybrid mobile
+          apps using React and Express.
+        </p>
+        <div className="project">
+          <Card sx={{ maxWidth: 385 }} className="proj">
+            <CardMedia
+              className="cimg1"
+              image="mypickup.png"
+              title="green iguana"
+            />
+            <CardContent className="card-con">
+              <Typography gutterBottom variant="h5" component="div">
+                Mypickup ( taxi booking app )
+              </Typography>
+              <Typography variant="body2"></Typography>
+            </CardContent>
+          </Card>
+          <Card sx={{ maxWidth: 385, minWidth: 345 }} className="proj">
+            <CardMedia
+              sx={{ minWidth: 285, minHeight: 220 }}
+              className="cimg1"
+              image="iwhizz.png"
+              title="green iguana"
+            />
+            <CardContent className="card-con">
+              <Typography gutterBottom variant="h5" component="div">
+                Iwhizz ( e-larning app )
+              </Typography>
+              <Typography variant="body2"></Typography>
+            </CardContent>
+          </Card>
+          <Card sx={{ maxWidth: 385 }} className="proj">
+            <CardMedia
+              sx={{ minWidth: 285, minHeight: 220 }}
+              className="cimg1"
+              image="ai.png"
+              title="green iguana"
+            />
+            <CardContent className="card-con">
+              <Typography gutterBottom variant="h5" component="div">
+                Aiempowerr ( business website )
+              </Typography>
+              <Typography variant="body2"></Typography>
+            </CardContent>
+          </Card>
+        </div>
       </div>
     </div>
   );
